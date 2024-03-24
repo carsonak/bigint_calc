6af9ac9ca34327520ea359dbbe2490e4a323174c
Andrew <49104171+carsonak@users.noreply.github.com>
2024-03-24 12:02:03 +0300

    Working tests

--- make_tests.mk
+++ make_tests.mk
@@ -1,8 +1,9 @@
 T_SRCDIR := $(TESTS_DIR)/src
 T_BINDIR := $(TESTS_DIR)/bin
-UNITY_SRCDIR := Unity/src $(shell find Unity/extras -name 'src' -type d | grep -E -ve '*fixture*')
+UNITY_SRCDIR := Unity/src $(shell find Unity/extras -name 'src' -type d | grep -E -ve '.*fixture.*')
+
 GENARATOR := Unity/auto/generate_test_runner.rb
-YAML_CONFIG := src/tests_config.yaml
+YAML_CONFIG := $(TESTS_DIR)/tests_config.yaml
 
 UNITY_SCRIPTS := $(shell find $(UNITY_SRCDIR) -mount -name '*.c' -type f)
 ASSERT_SCRIPTS := $(shell find tests/src -name "*.c" -type f | grep -E -ve '.*Runner.c')
@@ -12,18 +13,17 @@ T_BIN := $(ASSERT_SCRIPTS:$(T_SRCDIR)/%.c=$(T_BINDIR)/%)
 T_DEP := $(T_BIN:%=%.d)
 
 test: $(T_BIN)
-	@for xtest in $(T_BIN); do printf "./%s\n" $$xtest; ./$$xtest; echo >&2; done
+	@for xtest in $(T_BIN); do ./$$xtest; done
 
 # https://www.gnu.org/software/make/manual/html_node/Target_002dspecific.html
-$(T_BINDIR)/%: OBJ := $(filter-out %main.o,$(OBJ))
 $(T_BINDIR)/%: INC_FLAGS += $(addprefix -I,$(T_SRCDIR) $(UNITY_SRCDIR))
 
 $(T_BINDIR)/%: $(T_SRCDIR)/%.c $(T_SRCDIR)/%_Runner.c $(UNITY_SCRIPTS) $(OBJ)
-	@mkdir -p $(@D)
-	$(CC) $(CFLAGS) $^ -o $@ $(LDLIBS)
+	$(CC) $(CFLAGS) $(filter-out %.h %main.o,$^) -o $@ $(LDLIBS)
 
 $(T_SRCDIR)/%_Runner.c: $(T_SRCDIR)/%.c
-	./$(GENARATOR) ./$< ./$@
+	@./$(GENARATOR) ./$< ./$@
+	@echo 'Generated $@'
 
 tclean_bin:
 ifdef T_BINDIR
--- tests/src/tests.h
+++ tests/src/tests.h
@@ -1,13 +1,8 @@
 #ifndef _TESTS_H_
 #define _TESTS_H_
 
-#ifndef UNITY_INCLUDE_PRINT_FORMATTED
 #define UNITY_INCLUDE_PRINT_FORMATTED
-#endif
-
-#ifndef UNITY_USE_COMMAND_LINE_ARGS
-#define UNITY_USE_COMMAND_LINE_ARGS
-#endif
+/*#define UNITY_USE_COMMAND_LINE_ARGS*/
 
 #include "infiX.h"
 #include "unity.h"
